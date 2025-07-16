5e583297abc839088f408ffbbcdf27585652723d
wennhmann <info@wennhmann.de>
2025-07-16 19:27:04 +0200

    base ing the base

---
+++ src/do/structs/base.rs
@@ -0,0 +1,10 @@
+pub struct Base{
+    pub title:String,
+    pub status:String,
+}
+
+impl Base{
+    pub fn new(input_title:&String,input_status:&String){
+        Base{title:input_title.to_string(), status:input_status.to_string()}
+    }
+}
