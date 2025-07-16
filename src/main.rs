a7ad6b99cbe8be28bb0279ffd3f5ec27774563ea
wennhmann <info@wennhmann.de>
2025-07-16 22:39:47 +0200

    before

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod doit;
+use doit::structs::done::Done;
+use doit::structs::pending::Pending;
+
 fn main()  {
-  
+let done: Done = Done::new("programming");
+ println!("{}", done.super_struct.title);
+ println!("{}", done.super_struct.status);
+ let pending: Pending = Pending::new("teaching");
+ println!("{}", pending.super_struct.title);
+ println!("{}", pending.super_struct.status);
 }
