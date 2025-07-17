182a857cccb6df216ff986a080085116842d27a1
wennhmann <info@wennhmann.de>
2025-07-17 16:50:56 +0200

    traits edit and get

--- src/doit/structs/traits/edit.rs
+++ src/doit/structs/traits/edit.rs
@@ -1,9 +1,9 @@
 pub trait Edit{
     fn set_to_done(&self, title: &str){
-        println!("{} ::: set to done !!!!");
+        println!("{} ::: set to done !!!!",title);
     }
 
     fn set_to_pending(&self, title: &str){
-        println!("{} ::: set to pending !!!!");
+        println!("{} ::: set to pending !!!!",title);
     }
 }
