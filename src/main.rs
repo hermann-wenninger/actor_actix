948f03591a0f1a25aadf405731989410d658b6cb
wennhmann <info@wennhmann.de>
2025-07-17 16:02:33 +0200

    factory in action

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,18 @@ mod doit;
 use doit::structs::done::Done;
 use doit::structs::pending::Pending;
 
+use doit::ItemTypes;
+use doit::doit_factory;
+
 fn main()  {
+
+let doit_item:Result<ItemTypes, &'static str> = doit_factory("pending","find work");
+match doit_item.unwrap(){
+
+    ItemTypes::Pending(item)=>println!("Ist n pending item with the value::: {}",item.super_struct.title),
+    ItemTypes::Done(item)=>println!("Ist n done item with the value::: {}",item.super_struct.title),
+}
+
 let done: Done = Done::new("programming");
  println!("{}", done.super_struct.title);
  println!("{}", done.super_struct.status);
