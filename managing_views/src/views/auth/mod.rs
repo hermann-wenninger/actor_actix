c0e89d227d95445dcf64acd04eeee4271542c0a9
wennhmann <info@wennhmann.de>
2025-07-19 12:36:47 +0200

    compiled tutti due

--- managing_views/src/views/auth/mod.rs
+++ managing_views/src/views/auth/mod.rs
@@ -7,8 +7,8 @@ use super::path::Path;
 pub fn auth_factory(app: &mut web::ServiceConfig) {
     let base_path: Path = Path{path: String::from("/auth")};
 
-    app.route(&base_path.define(String::from("login")),
+    app.route(&base_path.define(&String::from("login")),
     web::get().to(login::login))
-    .route(&base_path.define(String::from("logout")),
+    .route(&base_path.define(&String::from("logout")),
     web::get().to(logout::logout));
 }
