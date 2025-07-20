793239bd466054e3f28cd8fc31f369b97cbab9dc
wennhmann <info@wennhmann.de>
2025-07-20 17:12:50 +0200

    basic setup

--- managing_views/src/views/mod.rs
+++ managing_views/src/views/mod.rs
@@ -2,7 +2,7 @@ use actix_web::web;
 mod path;
 mod auth;
 
-
 pub fn views_factory(app: &mut web::ServiceConfig) {
- auth::auth_factory(app);
+	let loggin_status: bool = true;
+	auth::auth_factory(app, loggin_status);
 }
