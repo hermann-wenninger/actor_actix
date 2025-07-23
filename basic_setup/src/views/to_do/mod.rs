2fb77fa448b0eb01f57c077d29c0e2ac3ea23e99
wennhmann <info@wennhmann.de>
2025-07-23 13:52:53 +0200

    change to post

--- basic_setup/src/views/to_do/mod.rs
+++ basic_setup/src/views/to_do/mod.rs
@@ -13,5 +13,5 @@ use super::path::Path;
 pub fn item_factory(app: &mut web::ServiceConfig) {
  let base_path: Path = Path{prefix: String::from("/item")};
  app.route(&base_path.define(String::from("/create/{title}")),
- web::post().to(create::create));
+ web::get().to(create::create));
 }
