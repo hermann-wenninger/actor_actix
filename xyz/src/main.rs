dfb2ee96fb419968d9562339843754b7ab931692
wennhmann <info@wennhmann.de>
2025-08-04 13:44:41 +0200

    server maybeee running

--- xyz/src/main.rs
+++ xyz/src/main.rs
@@ -1,8 +1,9 @@
 use std::borrow::Cow;
+//Cow clone on write
 fn main() {
 
     println!("Hello, world!");
-    let word = "cdefgahc";
+    let word: &str= "cdefgahc";
     let result = if word.chars().any(|a|a.is_lowercase()){
         Cow::Owned(word.to_uppercase())
      } else{
