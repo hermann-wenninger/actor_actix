392079509883aa0e328433b55cd8fa9398dbbe11
wennhmann <info@wennhmann.de>
2025-08-04 12:29:48 +0200

    connctet to rust server

--- ingress/frontend/src/index.tsx
+++ ingress/frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 const App = () => {
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1>Hello World..........</h1>
         </div>
     );
 };
